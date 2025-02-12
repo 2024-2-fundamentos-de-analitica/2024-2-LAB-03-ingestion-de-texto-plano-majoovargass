31a52b6618ca8dd61ca77949051ee8a1622134de
majoovargass <mvargasmor@unal.edu.co>
2025-02-12 15:54:19 -0500

    Lab3 Completado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,40 @@ def pregunta_01():
 
 
     """
+
+    import pandas as pd
+    import re
+	
+    inicio = dict()
+    valores = []
+    entrada = []
+    with open('./files/input/clusters_report.txt') as file:
+        for indicillo, line in enumerate(file):
+            if indicillo < 4:
+                # Catch header
+                coincidenciasdeencabezado = re.finditer(r'\w+(?:\s\w+)*', line)
+                for match in coincidenciasdeencabezado:
+                    indicillo = match.start()
+                    if indicillo not in inicio:
+                        inicio[indicillo] = []
+                    inicio[indicillo].append(match.group(0).lower())
+            else:
+                # Catch entry
+                numerillodecoincidencias = re.findall(r'\d+,*\d?', line)
+                if numerillodecoincidencias:
+                    entrada = []
+                    numerillos = [int(num) for num in numerillodecoincidencias[:2]] + [float(numerillodecoincidencias[2].replace(',', '.'))]
+                    entrada += numerillos + ['']
+                coincidenciasdetexto = re.findall(r'[a-zA-Z].+\n', line)
+                if coincidenciasdetexto:
+                    entrada[-1] += coincidenciasdetexto[0][:-1] + ' '
+                else:
+                    valores.append(entrada)
+    
+    encabezamiento = [' '.join(header_list) for header_list in inicio.values()]
+    valores = list(map(lambda x: x[:-1] + [' '.join(x[-1].split()).replace('.', '')], valores))
+    encabezamiento = list(map(lambda h: '_'.join(h.split()), encabezamiento))
+    dfp = pd.DataFrame(valores, columns=encabezamiento)
+
+    return dfp
+print(pregunta_01())
